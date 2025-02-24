d510b8cc54d3e682b13f0fe78585a5337fcb5cc4
Alexei Starovoitov <ast@kernel.org>
2025-02-24 14:16:37 -0800

    bpf: Fix deadlock between rcu_tasks_trace and event_mutex.
    BugLink: https://bugs.launchpad.net/bugs/2129307
    
    [ Upstream commit 4580f4e0ebdf8dc8d506ae926b88510395a0c1d1 ]
    
    Fix the following deadlock:
    CPU A
    _free_event()
      perf_kprobe_destroy()
        mutex_lock(&event_mutex)
          perf_trace_event_unreg()
            synchronize_rcu_tasks_trace()
    
    There are several paths where _free_event() grabs event_mutex
    and calls sync_rcu_tasks_trace. Above is one such case.
    
    CPU B
    bpf_prog_test_run_syscall()
      rcu_read_lock_trace()
        bpf_prog_run_pin_on_cpu()
          bpf_prog_load()
            bpf_tracing_func_proto()
              trace_set_clr_event()
                mutex_lock(&event_mutex)
    
    Delegate trace_set_clr_event() to workqueue to avoid
    such lock dependency.
    
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/bpf/20250224221637.4780-1-alexei.starovoitov@gmail.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    CVE-2025-37884
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -404,7 +404,7 @@ static const struct bpf_func_proto bpf_trace_printk_proto = {
 	.arg2_type	= ARG_CONST_SIZE,
 };
 
-static void __set_printk_clr_event(void)
+static void __set_printk_clr_event(struct work_struct *work)
 {
 	/*
 	 * This program might be calling bpf_trace_printk,
@@ -417,10 +417,11 @@ static void __set_printk_clr_event(void)
 	if (trace_set_clr_event("bpf_trace", "bpf_trace_printk", 1))
 		pr_warn_ratelimited("could not enable bpf_trace_printk events");
 }
+static DECLARE_WORK(set_printk_work, __set_printk_clr_event);
 
 const struct bpf_func_proto *bpf_get_trace_printk_proto(void)
 {
-	__set_printk_clr_event();
+	schedule_work(&set_printk_work);
 	return &bpf_trace_printk_proto;
 }
 
@@ -463,7 +464,7 @@ static const struct bpf_func_proto bpf_trace_vprintk_proto = {
 
 const struct bpf_func_proto *bpf_get_trace_vprintk_proto(void)
 {
-	__set_printk_clr_event();
+	schedule_work(&set_printk_work);
 	return &bpf_trace_vprintk_proto;
 }
 
