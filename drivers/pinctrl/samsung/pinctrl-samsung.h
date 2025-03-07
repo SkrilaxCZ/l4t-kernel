aff24aa42f36827d09a7ef39d9296596fd2fb00b
Peter Griffin <peter.griffin@linaro.org>
2025-03-07 10:29:05 +0000

    pinctrl: samsung: add support for eint_fltcon_offset
    BugLink: https://bugs.launchpad.net/bugs/2128722
    
    commit 701d0e910955627734917c3587258aa7e73068bb upstream.
    
    On gs101 SoC the fltcon0 (filter configuration 0) offset isn't at a
    fixed offset like previous SoCs as the fltcon1 register only exists when
    there are more than 4 pins in the bank.
    
    Add a eint_fltcon_offset and new GS101_PIN_BANK_EINT* macros that take
    an additional fltcon_offs variable.
    
    This can then be used in suspend/resume callbacks to save and restore
    the fltcon0 and fltcon1 registers.
    
    Fixes: 4a8be01a1a7a ("pinctrl: samsung: Add gs101 SoC pinctrl configuration")
    Cc: stable@vger.kernel.org
    Reviewed-by: André Draszik <andre.draszik@linaro.org>
    Signed-off-by: Peter Griffin <peter.griffin@linaro.org>
    Link: https://lore.kernel.org/r/20250307-pinctrl-fltcon-suspend-v4-1-2d775e486036@linaro.org
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- drivers/pinctrl/samsung/pinctrl-samsung.h
+++ drivers/pinctrl/samsung/pinctrl-samsung.h
@@ -125,6 +125,7 @@ struct samsung_pin_bank_type {
  * @eint_con_offset: ExynosAuto SoC-specific EINT control register offset of bank.
  * @eint_mask_offset: ExynosAuto SoC-specific EINT mask register offset of bank.
  * @eint_pend_offset: ExynosAuto SoC-specific EINT pend register offset of bank.
+ * @eint_fltcon_offset: GS101 SoC-specific EINT filter config register offset.
  * @name: name to be prefixed for each pin in this pin bank.
  */
 struct samsung_pin_bank_data {
@@ -139,6 +140,7 @@ struct samsung_pin_bank_data {
 	u32		eint_con_offset;
 	u32		eint_mask_offset;
 	u32		eint_pend_offset;
+	u32		eint_fltcon_offset;
 	const char	*name;
 };
 
@@ -156,6 +158,7 @@ struct samsung_pin_bank_data {
  * @eint_con_offset: ExynosAuto SoC-specific EINT register or interrupt offset of bank.
  * @eint_mask_offset: ExynosAuto SoC-specific EINT mask register offset of bank.
  * @eint_pend_offset: ExynosAuto SoC-specific EINT pend register offset of bank.
+ * @eint_fltcon_offset: GS101 SoC-specific EINT filter config register offset.
  * @name: name to be prefixed for each pin in this pin bank.
  * @id: id of the bank, propagated to the pin range.
  * @pin_base: starting pin number of the bank.
@@ -182,6 +185,7 @@ struct samsung_pin_bank {
 	u32		eint_con_offset;
 	u32		eint_mask_offset;
 	u32		eint_pend_offset;
+	u32		eint_fltcon_offset;
 	const char	*name;
 	u32		id;
 
