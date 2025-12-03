4ad1fb2172d20825f2e7e163e2f8e11e9383590b
Ketan Patil <ketanp@nvidia.com>
2025-12-03 08:27:44 +0000

    NVIDIA: mm: Create kconfig for max reclaim retries
    The value for maximum number of reclaim retries without progress before
    the OOM killer starts, is hardcoded in the current code. Instead of
    hardcoding, define a Kconfig to make it configurable. Higher values
    will cause the system to try harder to reclaim memory before invoking
    the OOM killer, which may increase latency and it can even result in
    hang and reboot during memory pressure situations.
    
    Bug 5602402
    
    Change-Id: I60dcf0d8d1689b3d548d2f6699bc8365a40601b5
    Signed-off-by: Ketan Patil <ketanp@nvidia.com>

--- mm/internal.h
+++ mm/internal.h
@@ -238,7 +238,7 @@ extern unsigned long highest_memmap_pfn;
  * Maximum number of reclaim retries without progress before the OOM
  * killer is consider the only way forward.
  */
-#define MAX_RECLAIM_RETRIES 16
+#define MAX_RECLAIM_RETRIES CONFIG_MAX_RECLAIM_RETRIES
 
 /*
  * in mm/vmscan.c:
